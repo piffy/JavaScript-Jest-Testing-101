77929a3235ef1fdc1cf1087e510fa2b5ab43684d
Prof. Missiroli (home) <prof.missiroli@gmail.com>
2021-11-23 15:58:23 +0100

    full tests

--- src/App.js
+++ src/App.js
@@ -19,14 +19,16 @@ const limiteScoperto=-500;
     }
 
     function preleva(somma){
-        if(saldo - somma >= limiteScoperto);{
+        if(saldo - somma > limiteScoperto){
             saldo -= somma;
             return somma;
         }
+        return 0;
+        
     }
 
     function versa(somma){
-        saldo + somma;
+        saldo += somma;
     }
 
     function estrattoConto() {
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import {init, test, estrattoConto} from './App';
+import {init, test, estrattoConto, versa, preleva} from './App';
 
 const descrivi=describe;
 const deve=it;
@@ -17,3 +17,46 @@ descrivi ('conto vuoto', () => {
         expect(estrattoConto()).toBe('Donald Trump: 0€');
     })
 })
+
+descrivi ('versamento', () => {
+    deve ('accettare un versamento', () => {
+        init('Donald Trump');
+        versa(100);
+        expect(estrattoConto()).toBe('Donald Trump: 100€');
+    });
+    deve ('accettare due versamenti', () => {
+        versa(50);
+        expect(estrattoConto()).toBe('Donald Trump: 150€');
+    });
+    
+})
+
+descrivi ('prelievo', () => {
+    deve ('accettare un prelievo', () => {
+        init('Donald Trump');
+        versa(100);
+        preleva(50);
+        expect(estrattoConto()).toBe('Donald Trump: 50€');
+    });
+
+    deve ('accettare un piccolo scoperto', () => {
+        init('Donald Trump');
+        versa(100);
+        let x=preleva(200);
+        expect(estrattoConto()).toBe('Donald Trump: -100€');
+        expect(x).toBe(200);
+    });
+    deve ('rifiutare uno scoperto eccessivo', () => {
+        init('Donald Trump');
+        versa(100);
+        let x=preleva(610);
+        expect(estrattoConto()).toBe('Donald Trump: 100€');
+        expect(x).toBe(0);
+
+    });
+
+
+    
+})
+
+
