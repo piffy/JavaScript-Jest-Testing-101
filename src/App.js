80e6ecfff4fae48e9fb312df057e7f732c47fafc
Prof. Missiroli (home) <prof.missiroli@gmail.com>
2021-11-24 17:04:57 +0100

    Inizio problema 2
    modifica base
    
    Spostamento vari
    
    Refactoring
    
    Soluzione
    
    Typo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-exports.init = init
-exports.preleva = preleva
-exports.versa = versa
-exports.estrattoConto = estrattoConto
-exports.test= test;
+exports.cesare = cesare
 
-
-var saldo=0,intestatario="";
-const limiteScoperto=-500;
-
-
-    function test() {
-        return "test";
-    }
-
-    function init(nome) {
-        intestatario=nome;
-        saldo=0;
-    }
-
-    function preleva(somma){
-        if(saldo - somma >= limiteScoperto);{
-            saldo -= somma;
-            return somma;
+    function cambia (lettera,spostamento) {
+        if (spostamento>26 || spostamento < -26) spostamento=0;
+        if (lettera>=65 && lettera<=90) {
+            lettera = lettera+spostamento;
+            lettera = (lettera >=91)  ?  lettera - 26 : lettera;
+            lettera = (lettera <65)  ?  lettera + 26 : lettera;
         }
+        if (lettera>=97 && lettera<=122) {
+            lettera = lettera+spostamento;
+            lettera = (lettera >=122)  ?  lettera - 26 : lettera;
+            lettera = (lettera <97)  ?  lettera + 26 : lettera;
+        }
+        return String.fromCharCode(lettera);
     }
 
-    function versa(somma){
-        saldo + somma;
-    }
-
-    function estrattoConto() {
-        return intestatario+": "+saldo+"€";
+    function cesare(str,spostamento) {
+        let retStr="";
+        if(typeof str!="string") return retStr;
+        if(spostamento==undefined) spostamento=0;
+        for (let c of str)
+            retStr=retStr+cambia(c.charCodeAt(0),spostamento)
+        return retStr;
     }
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,76 @@
-import {init, test, estrattoConto, versa, preleva} from './App';
+import {cesare} from './App';
 
-const descrivi=describe;
-const deve=it;
-var aspettatiChe=expect;
-
-
-descrivi ('test', () => {
-    deve ('restituire test', () => {
-        aspettatiChe(test()).toBe('test');
+describe ('la funzione esiste', () => {
+    test ('e restituisce una stringa', () => {
+        expect(typeof cesare()).toBe("string");
     })
 })
+
+describe ('non modifica', () => {
+    test ('la stringa se non richiesto', () => {
+        expect(cesare("A")).toBe("A");
+        expect(cesare("B")).toBe("B");
+    })
+})
+
+describe ('modifica base - 1', () => {
+    test ('sposta la lettera in avanti', () => {
+        expect(cesare("A",1)).toBe("B");
+        expect(cesare("B",1)).toBe("C");
+    })
+})
+
+describe ('modifica base - 2', () => {
+    test ('anche per la Z', () => {
+        expect(cesare("Z",1)).toBe("A");
+    })
+})
+
+describe ('spostamento multiplo', () => {
+    test ('di valori vari', () => {
+        expect(cesare("A",2)).toBe("C");
+        expect(cesare("B",3)).toBe("E");
+        expect(cesare("Z",4)).toBe("D");
+    })
+})
+
+
+describe ('decodifica', () => {
+    test ('con valori vari', () => {
+        expect(cesare("A",-1)).toBe("Z");
+        expect(cesare("B",-3)).toBe("Y");
+        expect(cesare("Z",-4)).toBe("V");
+    })
+})
+
+describe ('più di una lettera', () => {
+    test ('di valori vari', () => {
+        expect(cesare("ABCD",1)).toBe("BCDE");
+        expect(cesare("ABCDE",2)).toBe("CDEFG");
+        expect(cesare("ABC",-3)).toBe("XYZ");
+    })
+})
+
+describe ('raffinamento', () => {
+    test ('sposta le minuscole', () => {
+        expect(cesare("abcd",1)).toBe("bcde");
+        expect(cesare("abcde",2)).toBe("cdefg");
+        expect(cesare("abc",-3)).toBe("xyz");
+    })
+    test ('non spostare altro', () => {
+        expect(cesare("Hand, Cannot, Erase",1)).toBe("Iboe, Dboopu, Fsbtf");
+        expect(cesare("uno due tre?",2)).toBe("wpq fwg vtg?");
+        expect(cesare("òàùèé()",-3)).toBe("òàùèé()");
+    })
+    test ('robustezza (parametri strani)', () => {
+        expect(cesare("abcd",1000)).toBe("abcd");
+        expect(cesare("abcd",-1000)).toBe("abcd");
+        expect(cesare(12,-3)).toBe("");
+    })
+
+})
+
+
 
 
 
